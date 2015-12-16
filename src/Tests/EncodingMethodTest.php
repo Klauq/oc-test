8b213a4e326dc3fc5a3c29ff5dc6cc63266c2db7
Klauq <klaudia.gryf@vp.pl>
2015-12-16 21:50:00 +0100

    Update EncodingMethodTest.php

--- src/Tests/EncodingMethodTest.php
+++ src/Tests/EncodingMethodTest.php
@@ -1,7 +1,6 @@
+
 <?php
-
 namespace Tests;
-
 use Jkan\Component\Password\Encoder;
 use Jkan\Component\Password\Method\Md5Encoding;
 use KCygan\Password\Sha1Encoding;
@@ -9,23 +8,19 @@ use KCygan\Password\Sha1Encoding;
 class EncodingMethodTest extends \PHPUnit_Framework_TestCase
 {
     const PASSWORD = 'JakubKanclerz';
-
     public function testMd5Encoding()
     {
         $method = new Md5Encoding();
         $encoder = new Encoder($method);
-
         $this->assertEquals(
             'b9f68093fb2c77ffe0ce847bdc179e9a',
             $encoder->encodePassword(self::PASSWORD)
         );
     }
-
     public function testSha1Encoding()
     {
         $method = new Sha1Encoding();
         $encoder = new Encoder($method);
-
         $this->assertEquals(
             '2181b2633e7ae8c3e964317dbd591eee51ab7a6d',
             $encoder->encodePassword(self::PASSWORD)
